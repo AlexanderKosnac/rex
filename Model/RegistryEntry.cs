1883852d7fa38d38ceba3896cc80825eb76e6fed
Alexander Kosnac <alexander.kosnac@web.de>
2024-09-25 23:52:59 +0200

    Load Registry values into DataGrid

--- Model/RegistryEntry.cs
+++ Model/RegistryEntry.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace rex.Model
 {
     internal class RegistryEntry
     {
-        public string Value { get; set; }
+        public RegistryKey Key { get; set; }
+        public string KeyPath { get; set; }
+        public string ValueName { get; set; }
+        public object Value { get; set; }
+        public RegistryValueKind Kind { get; set; }
+
+        public RegistryEntry(RegistryKey Key, string ValueName)
+        {
+            this.Key = Key;
+            this.KeyPath = Key.ToString();
+            this.ValueName = ValueName;
+            this.Value = Key.GetValue(ValueName);
+            this.Kind = Key.GetValueKind(ValueName);
+        }
     }
 }
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Windows;
 
 namespace rex.ViewModel
 {
@@ -21,6 +23,60 @@ namespace rex.ViewModel
         public MainViewModel()
         {
             Entries = new ObservableCollection<RegistryEntry>();
+            FetchRegistryEntriesAsync();
+        }
+
+        private void FetchRegistryEntriesAsync()
+        {
+            Entries.Clear();
+            RegistryKey[] rootKeys = [
+                Registry.ClassesRoot,
+                Registry.CurrentUser,
+                Registry.LocalMachine,
+                Registry.Users,
+                Registry.CurrentConfig
+            ];
+
+            foreach (RegistryKey rootKey in rootKeys)
+            {
+                Console.WriteLine($"Root Hive: {rootKey.Name}");
+                TraverseRegistryKeys(rootKey, "");
+            }
+        }
+
+        private void TraverseRegistryKeys(RegistryKey rootKey, string subKeyPath)
+        {
+            List<RegistryEntry> ReList = [];
+
+            try
+            {
+                using (RegistryKey currentKey = subKeyPath == "" ? rootKey : rootKey.OpenSubKey(subKeyPath))
+                {
+                    if (currentKey != null)
+                    {
+                        foreach (string subKeyName in currentKey.GetSubKeyNames())
+                        {
+                            string fullSubKeyPath = subKeyPath == "" ? subKeyName : $"{subKeyPath}\\{subKeyName}";
+                            TraverseRegistryKeys(rootKey, fullSubKeyPath); // Recursion call
+                        }
+
+                        foreach (string valueName in currentKey.GetValueNames())
+                        {
+                            object value = currentKey.GetValue(valueName);
+                            ReList.Add(new RegistryEntry(currentKey, valueName));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error accessing subkey: {subKeyPath}, Exception: {ex.Message}");
+            }
+
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ReList.ForEach(Entries.Add);
+            });
         }
 
         private void OpenAbout()
