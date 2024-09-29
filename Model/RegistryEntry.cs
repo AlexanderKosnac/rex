2519d9020450b5326f2d150423f006124c30d960
Alexander Kosnac <alexander.kosnac@web.de>
2024-09-29 22:03:53 +0200

    Condense RegistryEntry constructor

--- Model/RegistryEntry.cs
+++ Model/RegistryEntry.cs
@@ -2,27 +2,19 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace rex.Model
 {
-    internal class RegistryEntry
+    internal class RegistryEntry(RegistryKey Key, string ValueName)
     {
-        public RegistryKey Key { get; set; }
-        public string KeyPath { get; set; }
-        public string ValueName { get; set; }
-        public object Value { get; set; }
-        public RegistryValueKind Kind { get; set; }
-
-        public RegistryEntry(RegistryKey Key, string ValueName)
-        {
-            this.Key = Key;
-            this.KeyPath = Key.ToString();
-            this.ValueName = ValueName;
-            this.Value = Key.GetValue(ValueName);
-            this.Kind = Key.GetValueKind(ValueName);
-        }
+        public RegistryKey Key { get; set; } = Key;
+        public string KeyPath { get; set; } = Key.ToString();
+        public string ValueName { get; set; } = ValueName == "" ? "(Default)" : ValueName;
+        public object? Value { get; set; } = Key.GetValue(ValueName);
+        public RegistryValueKind Kind { get; set; } = Key.GetValueKind(ValueName);
     }
 }
