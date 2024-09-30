ca50a36f1f7bd5a2303bcd772a30d6793faf0b0b
Alexander Kosnac <alexander.kosnac@web.de>
2024-09-30 20:16:31 +0200

    Type specific formatting for Binary and MultiString kind

--- Model/RegistryEntry.cs
+++ Model/RegistryEntry.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing.IndexedProperties;
 using System.Security.AccessControl;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -14,7 +16,27 @@ namespace rex.Model
         public RegistryKey Key { get; set; } = Key;
         public string KeyPath { get; set; } = Key.ToString();
         public string ValueName { get; set; } = ValueName == "" ? "(Default)" : ValueName;
-        public object? Value { get; set; } = Key.GetValue(ValueName);
+        public object? Value { get; set; } = RegistryEntry.getValue(Key, ValueName);
         public RegistryValueKind Kind { get; set; } = Key.GetValueKind(ValueName);
+
+        static object? getValue(RegistryKey Key, string ValueName)
+        {
+            object value = Key.GetValue(ValueName) ?? "";
+            switch (Key.GetValueKind(ValueName))
+            {
+                case RegistryValueKind.Binary:
+                    return System.Text.Encoding.UTF8.GetString((byte[]) value);
+                case RegistryValueKind.MultiString:
+                    return string.Join("", (string[]) value);
+                case RegistryValueKind.None:
+                case RegistryValueKind.Unknown:
+                case RegistryValueKind.String:
+                case RegistryValueKind.ExpandString:
+                case RegistryValueKind.DWord:
+                case RegistryValueKind.QWord:
+                default:
+                    return value;
+            }
+        }
     }
 }
